627d60a9f2edf663fb64459d19c7b2f9ac646fd3
Ravi <ravimansingh1234@gmail.com>
2021-07-09 19:56:25 +0900

    First commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+var menu= document.querySelector('.menu');
+var navigation = document.querySelector('.navigation');
+
+const toggle = () => {
+    menu.classList.toggle('active');
+    navigation.classList.toggle('active')
+};
